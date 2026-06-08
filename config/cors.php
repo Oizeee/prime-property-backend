36a2f5aa0d4fa3c87400e7e68e21c7d049ee04d7
Oizeee <fauziridwan173@gmail.com>
2026-06-08 17:11:52 +0700

    update cors

--- config/cors.php
+++ config/cors.php
@@ -2,14 +2,22 @@
 
 return [
 
+    /*
+    |--------------------------------------------------------------------------
+    | Cross-Origin Resource Sharing (CORS) Configuration
+    |--------------------------------------------------------------------------
+    |
+    | Here you may configure your settings for cross-origin resource sharing
+    | or "CORS". This determines what cross-origin operations may execute
+    | in web browsers. You are free to adjust these settings as needed.
+    |
+    */
+
     'paths' => ['api/*', 'sanctum/csrf-cookie'],
 
     'allowed_methods' => ['*'],
 
-    'allowed_origins' => [
-        'http://localhost:3000',
-        'https://prime-property-frontend-three.vercel.app',
-    ],
+    'allowed_origins' => ['*'], 
 
     'allowed_origins_patterns' => [],
 
@@ -19,6 +27,6 @@ return [
 
     'max_age' => 86400,
 
-    'supports_credentials' => false,
+    'supports_credentials' => false, 
 
 ];
